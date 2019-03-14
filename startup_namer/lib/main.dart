f8b859bd60c26b4a6a98f37f729d82efc53286b2
Usheninte <jrandangana@gmail.com>
2019-03-14 19:35:05 +0100

    updated startup_namer

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center( // Change "const" to "new"
+          // child: const Text('Hello World'), // Replace this text...
+          child: new Text(wordPair.asPascalCase) // With this text.
         ),
       ),
     );
